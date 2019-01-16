d2b7a0b42ad68944bb3b2ce92f8855c4ea9a5d42
Private <private@Jakubs-MacBook-Pro-6.local>
2019-01-16 21:22:19 +0000

    14.4 course

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,35 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("v1/task")
+public class TaskController {
+    @RequestMapping(method =  RequestMethod.GET, value = "getTasks")
+    public List<TaskDto> getTasks() {
+        return new ArrayList<>();
+    }
+
+    public TaskDto getTask() {
+        return new TaskDto(1L, "Title", "content");
+    }
+
+    public void deleteTask() {
+
+    }
+
+    public TaskDto updateTask(TaskDto taskDto) {
+        return new TaskDto(1L, "Edited Title", "Edited content");
+    }
+
+    public void createTask(TaskDto taskDto) {
+
+    }
+}
