808e2af709ffa002595cfb1972443f616d5cb6eb
jakub.tadych@gmail.com <!Madzia1980>
2019-02-10 22:45:01 +0000

    refactory

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -27,25 +27,7 @@ public class TrelloController {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
-    public void getTrelloBoards() {
-
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-
-//        trelloBoards
-//                .stream()
-//                .filter(trelloBoardDto -> trelloBoardDto.getId().length() > 0)
-//                .filter(trelloBoardDto -> trelloBoardDto.getName().length() > 0)
-//                .filter(trelloBoardDto -> trelloBoardDto.getName().contains("Kodilla"))
-//                .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
-
-        trelloBoards.forEach(trelloBoardDto->{
-            System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
-
-            System.out.println("This board contains lists: ");
-
-            trelloBoardDto.getLists().forEach(trelloList ->
-                    System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
-
-        });
+    public List<TrelloBoardDto> getTrelloBoards() {
+        return trelloClient.getTrelloBoards();
     }
 }
